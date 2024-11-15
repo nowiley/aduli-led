37efdec5544bbb9ef7da709900f6339527d4267a
Luc Gaitskell <me@lucg.xyz>
2024-11-14 22:41:58 -0500

    Use ruff formatting on python lol

--- sim/test_led_driver.py
+++ sim/test_led_driver.py
@@ -1,13 +1,12 @@
-import cocotb
 import os
 import sys
-from math import log
-import logging
 from pathlib import Path
+
+import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import Timer, ClockCycles, RisingEdge, FallingEdge, ReadOnly,with_timeout
-from cocotb.utils import get_sim_time as gst
 from cocotb.runner import get_runner
+from cocotb.triggers import ClockCycles, FallingEdge, RisingEdge
+
 
 @cocotb.test()
 async def test_a(dut):
@@ -15,7 +14,7 @@ async def test_a(dut):
     dut._log.info("Starting...")
     cocotb.start_soon(Clock(dut.clk_in, 10, units="ns").start())
     dut.rst_in.value = 1
-    await ClockCycles(dut.clk_in,3)
+    await ClockCycles(dut.clk_in, 3)
     await FallingEdge(dut.clk_in)
     dut.rst_in.value = 0
     dut.green_in.value = 0x00
@@ -28,18 +27,20 @@ async def test_a(dut):
     await ClockCycles(dut.clk_in, 3)
     assert dut.ready_out.value == 1, "Should still be ready after a few cycles"
     assert dut.strand_out.value == 0, "Strand should be low if not sending data"
-    dut.data_in.valid = 1 # start sending data
+    dut.data_in.valid = 1  # start sending data
     await ClockCycles(dut.clk_in, 1)
     dut.data_in.valid = 0
     dut._log.info("Checking correct protocol")
     bitstring = 0b00000000_10101010_00000000
-# // Assuming 100MHz clock, 10ns period ->
-# // 0 bit = 0.4us high, 0.85us low -> 40 cycles high, 85 cycles low
-# // 1 bit = 0.8us high, 0.45us low -> 80 cycles high, 45 cycles low
-# // reset = 50us low
-    assert dut.strand_out.value == 1, "Strand should be high immediately after valid data in"
+    # // Assuming 100MHz clock, 10ns period ->
+    # // 0 bit = 0.4us high, 0.85us low -> 40 cycles high, 85 cycles low
+    # // 1 bit = 0.8us high, 0.45us low -> 80 cycles high, 45 cycles low
+    # // reset = 50us low
+    assert (
+        dut.strand_out.value == 1
+    ), "Strand should be high immediately after valid data in"
     for i in range(25):
-        cur_bit = (bitstring >> (24-i)) & 0x01
+        cur_bit = (bitstring >> (24 - i)) & 0x01
         dut._log.info(f"Checking bit {i} = {cur_bit}")
         assert dut.strand_out.value == 1, "Strand should be high at the start"
         if cur_bit == 0:
@@ -79,16 +80,17 @@ def is_runner():
         always=True,
         build_args=build_test_args,
         parameters=parameters,
-        timescale = ('1ns','1ps'),
-        waves=True
+        timescale=("1ns", "1ps"),
+        waves=True,
     )
     run_test_args = []
     runner.test(
         hdl_toplevel="led_driver",
         test_module="test_led_driver",
         test_args=run_test_args,
-        waves=True
+        waves=True,
     )
+
 
 if __name__ == "__main__":
     is_runner()
