effc7dd8e1161957dfd737624cdf65f778a15643
Luc Gaitskell <me@lucg.xyz>
2024-11-14 22:44:40 -0500

    Auto-format event counter

--- hdl/common/evt_counter.sv
+++ hdl/common/evt_counter.sv
@@ -1,6 +1,6 @@
 `default_nettype none
 module evt_counter #(
-    parameter MAX_COUNT = 255
+    parameter int MAX_COUNT = 255
 ) (
     input  wire                          clk_in,
     input  wire                          rst_in,
@@ -8,14 +8,14 @@ module evt_counter #(
     output logic [$clog2(MAX_COUNT)-1:0] count_out
 );
 
-  always_ff @(posedge clk_in) begin
-    if (rst_in || (count_out == MAX_COUNT - 1 && evt_in)) begin
-      count_out <= 16'b0;
-    end else begin
-      if (evt_in) begin
-        count_out <= count_out + 1;
-      end
+    always_ff @(posedge clk_in) begin
+        if (rst_in || (count_out == MAX_COUNT - 1 && evt_in)) begin
+            count_out <= 16'b0;
+        end else begin
+            if (evt_in) begin
+                count_out <= count_out + 1;
+            end
+        end
     end
-  end
 endmodule
 `default_nettype wire
