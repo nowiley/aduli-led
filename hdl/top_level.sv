f18d178a0f59ad9606680546df8ce2910e516416
Noah Wiley <noahjwiley22@gmail.com>
2024-11-21 22:29:11 -0500

    added most stuff from lab 5 need to test building

--- hdl/top_level.sv
+++ hdl/top_level.sv
@@ -224,48 +224,385 @@ module top_level #(
     .cb_out(cb_full)
     );
 
+    // VIDEO MUX STUFF
+    //channel select module (select which of six color channels to mask):
+    logic [2:0] channel_sel;
+    logic [7:0] selected_channel; //selected channels
+    //selected_channel could contain any of the six color channels depend on selection
 
+    //threshold module (apply masking threshold):
+    logic [7:0] lower_threshold;
+    logic [7:0] upper_threshold;
+    logic mask; //Whether or not thresholded pixel is 1 or 0
 
+    // SKIPPNG COM
 
-
-
-
-
-    wire rst_in = btn[0];
-
-    logic [COLOR_WIDTH-1:0] next_red, next_green, next_blue;
-    logic color_valid;
-    logic [CounterWidth-1:0] next_led_request;
-
-    // instantiate pattern modules
-    pat_gradient #(
-        .NUM_LEDS(NUM_LEDS),
-        .COLOR_WIDTH(COLOR_WIDTH)
-    ) pat_gradient_inst (
-        .rst_in(rst_in),
-        .clk_in(clk_100mhz),
-        .next_led_request(next_led_request),
-        .red_out(next_red),
-        .green_out(next_green),
-        .blue_out(next_blue),
-        .color_valid(color_valid)
+    //take lower 8 of full outputs.
+    // treat cr and cb as signed numbers, invert the MSB to get an unsigned equivalent ( [-128,128) maps to [0,256) )
+    assign y = y_full[7:0];
+    assign cr = {!cr_full[7],cr_full[6:0]};
+    assign cb = {!cb_full[7],cb_full[6:0]};
+    assign channel_sel = sw[3:1];
+    // * 3'b000: green
+    // * 3'b001: red
+    // * 3'b010: blue
+    // * 3'b011: not valid
+    // * 3'b100: y (luminance)
+    // * 3'b101: Cr (Chroma Red)
+    // * 3'b110: Cb (Chroma Blue)
+    // * 3'b111: not valid
+    //Channel Select: Takes in the full RGB and YCrCb information and
+    // chooses one of them to output as an 8 bit value
+    localparam PS1_STAGES = 3;
+    logic [7:0] ps1_r_in [PS1_STAGES-1:0];
+    logic [7:0] ps1_g_in [PS1_STAGES-1:0];
+    logic [7:0] ps1_b_in [PS1_STAGES-1:0];
+    always_ff @(posedge clk_pixel)begin
+        ps1_r_in[0] <= fb_red;
+        ps1_g_in[0] <= fb_green;
+        ps1_b_in[0] <= fb_blue;
+        for (int i=1; i<PS1_STAGES; i = i+1)begin
+            ps1_r_in[i] <= ps1_r_in[i-1];
+            ps1_g_in[i] <= ps1_g_in[i-1];
+            ps1_b_in[i] <= ps1_b_in[i-1];
+        end
+    end
+    channel_select mcs(
+        .sel_in(channel_sel),
+        .r_in(ps1_r_in[PS1_STAGES-1]),    //TODO: needs to use pipelined signal (PS1)
+        .g_in(ps1_g_in[PS1_STAGES-1]),  //TODO: needs to use pipelined signal (PS1)
+        .b_in(ps1_b_in[PS1_STAGES-1]),   //TODO: needs to use pipelined signal (PS1)
+        .y_in(y),
+        .cr_in(cr),
+        .cb_in(cb),
+        .channel_out(selected_channel)
     );
 
-    // instantiate led_driver module
-    led_driver #(
-        .NUM_LEDS(NUM_LEDS),
-        .COLOR_WIDTH(COLOR_WIDTH)
-    ) led_driver_inst (
-        .rst_in(rst_in),
-        .clk_in(clk_100mhz),
-        .force_reset(btn[1]),
-        .green_in(next_green),
-        .red_in(next_red),
-        .blue_in(next_blue),
-        .color_valid(color_valid),
-        .strand_out(strand_out[0]),
-        .next_led_request(next_led_request)
+    //threshold values used to determine what value  passes:
+    assign lower_threshold = {sw[11:8],4'b0};
+    assign upper_threshold = {sw[15:12],4'b0};
+
+    //Thresholder: Takes in the full selected channedl and
+    //based on upper and lower bounds provides a binary mask bit
+    // * 1 if selected channel is within the bounds (inclusive)
+    // * 0 if selected channel is not within the bounds
+    threshold mt(
+        .clk_in(clk_pixel),
+        .rst_in(sys_rst_pixel),
+        .pixel_in(selected_channel),
+        .lower_bound_in(lower_threshold),
+        .upper_bound_in(upper_threshold),
+        .mask_out(mask) //single bit if pixel within mask.
     );
+
+    // SKIP Seven Segment display
+
+    // SKIP COM
+
+    // SKIP IMAGE SPRITE
+
+    // SKIP Crosshair overlay
+
+    // HDMI
+    // HDMI video signal generator
+    video_sig_gen vsg(
+        .pixel_clk_in(clk_pixel),
+        .rst_in(sys_rst_pixel),
+        .hcount_out(hcount_hdmi),
+        .vcount_out(vcount_hdmi),
+        .vs_out(vsync_hdmi),
+        .hs_out(hsync_hdmi),
+        .nf_out(nf_hdmi),
+        .ad_out(active_draw_hdmi),
+        .fc_out(frame_count_hdmi)
+    );
+
+    // Video Mux: select from the different display modes based on switch values
+    //used with switches for display selections
+    logic [1:0] display_choice;
+    logic [1:0] target_choice;
+
+    assign display_choice = sw[5:4];
+    assign target_choice =  sw[7:6];
+
+    localparam PS2_STAGES = 4;
+    // PS3 defined abover
+    // PS4 not needed
+    localparam PS5_STAGES = 1;
+    localparam PS6_STAGES = 1;
+    // PS7 not needed
+    localparam PS8_STAGES = 8;
+    localparam PS9_STAGES = 4;
+
+    // PS2
+    logic [7:0] ps2_fb_red [PS2_STAGES-1:0];
+    logic [7:0] ps2_fb_green [PS2_STAGES-1:0];
+    logic [7:0] ps2_fb_blue [PS2_STAGES-1:0];
+    always_ff @(posedge clk_pixel)begin
+        ps2_fb_red[0] <= fb_red;
+        ps2_fb_green[0] <= fb_green;
+        ps2_fb_blue[0] <= fb_blue;
+        for (int i=1; i<PS2_STAGES; i = i+1)begin
+            ps2_fb_red[i] <= ps2_fb_red[i-1];
+            ps2_fb_green[i] <= ps2_fb_green[i-1];
+            ps2_fb_blue[i] <= ps2_fb_blue[i-1];
+        end
+    end
+
+    // PS6
+    logic [9:0] ps6_y [PS6_STAGES-1:0];
+    always_ff @(posedge clk_pixel)begin
+        ps6_y[0] <= y;
+        for (int i=1; i<PS6_STAGES; i = i+1)begin
+            ps6_y[i] <= ps6_y[i-1];
+        end
+    end
+
+    //PS5
+    logic [7:0] ps5_selected_channel [PS5_STAGES-1:0];
+    always_ff @(posedge clk_pixel)begin
+        ps5_selected_channel[0] <= selected_channel;
+        for (int i=1; i<PS5_STAGES; i = i+1)begin
+            ps5_selected_channel[i] <= ps5_selected_channel[i-1];
+        end
+    end
+
+    //PS8 
+    logic [7:0] ps8_ch_red [PS8_STAGES-1:0];
+    logic [7:0] ps8_ch_green [PS8_STAGES-1:0];
+    logic [7:0] ps8_ch_blue [PS8_STAGES-1:0];
+    always_ff @(posedge clk_pixel)begin
+        ps8_ch_red[0] <= ch_red;
+        ps8_ch_green[0] <= ch_green;
+        ps8_ch_blue[0] <= ch_blue;
+        for (int i=1; i<PS8_STAGES; i = i+1)begin
+            ps8_ch_red[i] <= ps8_ch_red[i-1];
+            ps8_ch_green[i] <= ps8_ch_green[i-1];
+            ps8_ch_blue[i] <= ps8_ch_blue[i-1];
+        end
+    end
+
+    //PS9
+    logic [7:0] ps9_img_red [PS9_STAGES-1:0];
+    logic [7:0] ps9_img_green [PS9_STAGES-1:0];
+    logic [7:0] ps9_img_blue [PS9_STAGES-1:0];
+    always_ff @(posedge clk_pixel)begin
+        ps9_img_red[0] <= img_red;
+        ps9_img_green[0] <= img_green;
+        ps9_img_blue[0] <= img_blue;
+        for (int i=1; i<PS9_STAGES; i = i+1)begin
+            ps9_img_red[i] <= ps9_img_red[i-1];
+            ps9_img_green[i] <= ps9_img_green[i-1];
+            ps9_img_blue[i] <= ps9_img_blue[i-1];
+        end
+    end
+
+    video_mux mvm(
+        .bg_in(display_choice), //choose background
+        .target_in(target_choice), //choose target
+        .camera_pixel_in({ps2_fb_red[PS2_STAGES-1], ps2_fb_green[PS2_STAGES-1], ps2_fb_blue[PS2_STAGES-1]}), //TODO: needs (PS2)
+        .camera_y_in(ps6_y[PS6_STAGES-1]), //luminance TODO: needs (PS6)
+        .channel_in(ps5_selected_channel[PS5_STAGES-1]), //current channel being drawn TODO: needs (PS5)
+        .thresholded_pixel_in(mask), //one bit mask signal TODO: needs (PS4)
+        .crosshair_in({ps8_ch_red[PS8_STAGES-1], ps8_ch_green[PS8_STAGES-1], ps8_ch_blue[PS8_STAGES-1]}), //TODO: needs (PS8)
+        .com_sprite_pixel_in({ps9_img_red[PS9_STAGES-1], ps9_img_green[PS9_STAGES-1], ps9_img_blue[PS9_STAGES-1]}), //TODO: needs (PS9) maybe?
+        .pixel_out({red,green,blue}) //output to tmds
+    );
+
+    // HDMI Output: just like before!
+
+    logic [9:0] tmds_10b [0:2]; //output of each TMDS encoder!
+    logic       tmds_signal [2:0]; //output of each TMDS serializer!
+
+    //three tmds_encoders (blue, green, red)
+    //note green should have no control signal like red
+    //the blue channel DOES carry the two sync signals:
+    //  * control_in[0] = horizontal sync signal
+    //  * control_in[1] = vertical sync signal
+
+    tmds_encoder tmds_red(
+        .clk_in(clk_pixel),
+        .rst_in(sys_rst_pixel),
+        .data_in(red),
+        .control_in(2'b0),
+        .ve_in(ps3_ad[PS3_STAGES-1]),
+        .tmds_out(tmds_10b[2]));
+
+    tmds_encoder tmds_green(
+            .clk_in(clk_pixel),
+            .rst_in(sys_rst_pixel),
+            .data_in(green),
+            .control_in(2'b0),
+            .ve_in(ps3_ad[PS3_STAGES-1]),
+            .tmds_out(tmds_10b[1]));
+
+    tmds_encoder tmds_blue(
+        .clk_in(clk_pixel),
+        .rst_in(sys_rst_pixel),
+        .data_in(blue),
+        .control_in({ps3_vsync[PS3_STAGES-1],ps3_hsync[PS3_STAGES-1]}),
+        .ve_in(ps3_ad[PS3_STAGES-1]),
+        .tmds_out(tmds_10b[0]));
+
+
+    //three tmds_serializers (blue, green, red):
+    //MISSING: two more serializers for the green and blue tmds signals.
+    tmds_serializer red_ser(
+            .clk_pixel_in(clk_pixel),
+            .clk_5x_in(clk_5x),
+            .rst_in(sys_rst_pixel),
+            .tmds_in(tmds_10b[2]),
+            .tmds_out(tmds_signal[2]));
+    tmds_serializer green_ser(
+            .clk_pixel_in(clk_pixel),
+            .clk_5x_in(clk_5x),
+            .rst_in(sys_rst_pixel),
+            .tmds_in(tmds_10b[1]),
+            .tmds_out(tmds_signal[1]));
+    tmds_serializer blue_ser(
+            .clk_pixel_in(clk_pixel),
+            .clk_5x_in(clk_5x),
+            .rst_in(sys_rst_pixel),
+            .tmds_in(tmds_10b[0]),
+            .tmds_out(tmds_signal[0]));
+
+    //output buffers generating differential signals:
+    //three for the r,g,b signals and one that is at the pixel clock rate
+    //the HDMI receivers use recover logic coupled with the control signals asserted
+    //during blanking and sync periods to synchronize their faster bit clocks off
+    //of the slower pixel clock (so they can recover a clock of about 742.5 MHz from
+    //the slower 74.25 MHz clock)
+    OBUFDS OBUFDS_blue (.I(tmds_signal[0]), .O(hdmi_tx_p[0]), .OB(hdmi_tx_n[0]));
+    OBUFDS OBUFDS_green(.I(tmds_signal[1]), .O(hdmi_tx_p[1]), .OB(hdmi_tx_n[1]));
+    OBUFDS OBUFDS_red  (.I(tmds_signal[2]), .O(hdmi_tx_p[2]), .OB(hdmi_tx_n[2]));
+    OBUFDS OBUFDS_clock(.I(clk_pixel), .O(hdmi_clk_p), .OB(hdmi_clk_n));
+
+
+    // Nothing To Touch Down Here:
+    // register writes to the camera
+
+    // The OV5640 has an I2C bus connected to the board, which is used
+    // for setting all the hardware settings (gain, white balance,
+    // compression, image quality, etc) needed to start the camera up.
+    // We've taken care of setting these all these values for you:
+    // "rom.mem" holds a sequence of bytes to be sent over I2C to get
+    // the camera up and running, and we've written a design that sends
+    // them just after a reset completes.
+
+    // If the camera is not giving data, press your reset button.
+
+    logic  busy, bus_active;
+    logic  cr_init_valid, cr_init_ready;
+
+    logic  recent_reset;
+    always_ff @(posedge clk_camera) begin
+        if (sys_rst_camera) begin
+            recent_reset <= 1'b1;
+            cr_init_valid <= 1'b0;
+        end
+        else if (recent_reset) begin
+            cr_init_valid <= 1'b1;
+            recent_reset <= 1'b0;
+        end else if (cr_init_valid && cr_init_ready) begin
+            cr_init_valid <= 1'b0;
+        end
+    end
+
+    logic [23:0] bram_dout;
+    logic [7:0]  bram_addr;
+
+    // ROM holding pre-built camera settings to send
+    xilinx_single_port_ram_read_first
+        #(
+        .RAM_WIDTH(24),
+        .RAM_DEPTH(256),
+        .RAM_PERFORMANCE("HIGH_PERFORMANCE"),
+        .INIT_FILE("rom.mem")
+        ) registers
+        (
+        .addra(bram_addr),     // Address bus, width determined from RAM_DEPTH
+        .dina(24'b0),          // RAM input data, width determined from RAM_WIDTH
+        .clka(clk_camera),     // Clock
+        .wea(1'b0),            // Write enable
+        .ena(1'b1),            // RAM Enable, for additional power savings, disable port when not in use
+        .rsta(sys_rst_camera), // Output reset (does not affect memory contents)
+        .regcea(1'b1),         // Output register enable
+        .douta(bram_dout)      // RAM output data, width determined from RAM_WIDTH
+        );
+
+    logic [23:0] registers_dout;
+    logic [7:0]  registers_addr;
+    assign registers_dout = bram_dout;
+    assign bram_addr = registers_addr;
+
+    logic       con_scl_i, con_scl_o, con_scl_t;
+    logic       con_sda_i, con_sda_o, con_sda_t;
+
+    // NOTE these also have pullup specified in the xdc file!
+    // access our inouts properly as tri-state pins
+    IOBUF IOBUF_scl (.I(con_scl_o), .IO(i2c_scl), .O(con_scl_i), .T(con_scl_t) );
+    IOBUF IOBUF_sda (.I(con_sda_o), .IO(i2c_sda), .O(con_sda_i), .T(con_sda_t) );
+
+    // provided module to send data BRAM -> I2C
+    camera_registers crw
+        (.clk_in(clk_camera),
+        .rst_in(sys_rst_camera),
+        .init_valid(cr_init_valid),
+        .init_ready(cr_init_ready),
+        .scl_i(con_scl_i),
+        .scl_o(con_scl_o),
+        .scl_t(con_scl_t),
+        .sda_i(con_sda_i),
+        .sda_o(con_sda_o),
+        .sda_t(con_sda_t),
+        .bram_dout(registers_dout),
+        .bram_addr(registers_addr));
+
+    // a handful of debug signals for writing to registers
+    assign led[0] = crw.bus_active;
+    assign led[1] = cr_init_valid;
+    assign led[2] = cr_init_ready;
+    assign led[15:3] = 0;
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// LED STUFF
+wire rst_in = btn[0];
+
+logic [COLOR_WIDTH-1:0] next_red, next_green, next_blue;
+logic color_valid;
+logic [CounterWidth-1:0] next_led_request;
+
+// instantiate pattern modules
+pat_gradient #(
+    .NUM_LEDS(NUM_LEDS),
+    .COLOR_WIDTH(COLOR_WIDTH)
+) pat_gradient_inst (
+    .rst_in(rst_in),
+    .clk_in(clk_100mhz),
+    .next_led_request(next_led_request),
+    .red_out(next_red),
+    .green_out(next_green),
+    .blue_out(next_blue),
+    .color_valid(color_valid)
+);
+
+// instantiate led_driver module
+led_driver #(
+    .NUM_LEDS(NUM_LEDS),
+    .COLOR_WIDTH(COLOR_WIDTH)
+) led_driver_inst (
+    .rst_in(rst_in),
+    .clk_in(clk_100mhz),
+    .force_reset(btn[1]),
+    .green_in(next_green),
+    .red_in(next_red),
+    .blue_in(next_blue),
+    .color_valid(color_valid),
+    .strand_out(strand_out[0]),
+    .next_led_request(next_led_request)
+);
 
 
 endmodule
