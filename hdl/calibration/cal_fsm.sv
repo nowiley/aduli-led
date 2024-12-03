7bf172cd586f89794a5e9d3097ea227891569dff
Noah Wiley <noahjwiley22@gmail.com>
2024-12-03 17:42:06 -0500

    added dual port ram

--- hdl/calibration/cal_fsm.sv
+++ hdl/calibration/cal_fsm.sv
@@ -34,9 +34,34 @@ module cal_fsm
         input wire [15:0] frame_buffer_data
 );
 
+logic [CAL_TABLE_COUNTER_WIDTH-1:0] internal_read_request_address;
+logic [LED_ADDRESS_WIDTH:0]         internal_read_data;
+logic [LED_ADDRESS_WIDTH:0]         internal_write_data;
+logic                               internal_write_enable;
+
 // Instantiate the calibration table bram
 // 2 Port so that we can write internally and allow hdmi to read externally
-
+xilinx_true_dual_port_read_first_2_clock_ram 
+#(
+   .RAM_WIDTH = LED_ADDRESS_WIDTH + 1,
+   .RAM_DEPTH = NUM_FRAME_BUFFER_PIXELS
+) cal_table_bram (
+    .clka(clk),
+    // INTERNAL WRITE/READ to update calibration table
+    .addra(internal_read_request_address), // internal read address  
+    .douta(internal_read_data), // internal read data
+    .wea(internal_write_enable), // internal write enable
+    .dina(internal_write_data), // internal write data
+    // EXTERNAL READ to read calibration table
+    .addrb(cal_table_read_request_address), // external read address
+    .doutb(cal_table_read_data) // external read data
+    .ena(1'b1), // enable ram A
+    .enb(1'b1) // enable ram B
+    .rsta(rst), // reset ram A
+    .rstb(rst) // reset ram B
+    .regcea(1'b1), // register enable A
+    .regceb(1'b1) // register enable B
+);
 
 
 
