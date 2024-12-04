32c34b89702e9cc1b5a60432d8f5a13e6ba252b0
Noah Wiley <noahjwiley22@gmail.com>
2024-12-04 01:11:15 -0500

    pass test

--- hdl/calibration/calibration_fsm_w_accum.sv
+++ hdl/calibration/calibration_fsm_w_accum.sv
@@ -1,8 +1,14 @@
 `timescale 1ns / 1ps
-`include "mem/xilinx_true_dual_port_read_first_2_clock_ram.v"
-`include "calibration/id_shower.sv"
 `include "mem/shift_accum_ram.sv"
 `default_nettype none
+
+typedef enum logic [2:0] {
+    IDLE = 0,
+    WAIT_FOR_LED_STRAND_VALID = 1,
+    WAIT_FOR_CAM = 2,
+    WAIT_FOR_NFRAME = 3,
+    CAPTURE_FRAME = 4
+} fsm_state_t;
 
 module calibration_fsm_w_accum 
 #(
@@ -12,7 +18,8 @@ module calibration_fsm_w_accum
     parameter int WAIT_CYCLES = 10000000,
     parameter int ACTIVE_H_PIXELS = 320,
     parameter int ACTIVE_LINES = 180,
-    localparam int WAIT_COUNTER_WIDTH = $clog2(WAIT_CYCLES)
+    localparam int WAIT_COUNTER_WIDTH = $clog2(WAIT_CYCLES),
+    localparam int ADDRB_DEPTH_WIDTH = $clog2(ACTIVE_H_PIXELS * ACTIVE_LINES)
 )(
         input wire clk_pixel,
         input wire rst,
@@ -28,36 +35,29 @@ module calibration_fsm_w_accum
         input wire [9:0] vcount_in,
         input wire new_frame_in,
         input wire detect_0,
-        input wire detect_1
+        input wire detect_1,
+        output fsm_state_t state,
+        output logic [LED_ADDRESS_WIDTH-1:0] read_out
+
 );
 
-logic enum {
-    IDLE,
-    WAIT_FOR_LED_STRAND_VALID,
-    WAIT_FOR_CAM,
-    WAIT_FOR_NFRAME,
-    CAPTURE_FRAME
-} state;
 
 logic old_nf;
-logic old_displayed_frame_valid;
 logic old_increment_id;
 logic [WAIT_COUNTER_WIDTH-1:0] wait_counter;
 wire active_draw = (hcount_in < ACTIVE_H_PIXELS && vcount_in < ACTIVE_LINES) && !rst;
 wire top_left = ((hcount_in[1:0] == 2'b00) && (vcount_in[1:0] == 2'b00));
 wire good_addrb = top_left && active_draw;
-wire addrb = (319 - (hcount_in >> 2)) + 320 * (vcount_in >> 2);
+wire [ADDRB_DEPTH_WIDTH-1:0] addrb = (hcount_in >> 2) + (ACTIVE_H_PIXELS>>2) * (vcount_in >> 2);
 
 always_ff @(posedge clk_pixel) begin
     if (rst) begin
         state <= IDLE;
         wait_counter <= 0;
         old_nf <= 0;
-        old_displayed_frame_valid <= 0;
         old_increment_id <= 0;
     end else begin
         old_nf <= new_frame_in;
-        old_displayed_frame_valid <= displayed_frame_valid;
         old_increment_id <= increment_id;
 
         case (state)
@@ -67,7 +67,7 @@ always_ff @(posedge clk_pixel) begin
                 end
             end
             WAIT_FOR_LED_STRAND_VALID: begin
-                if (displayed_frame_valid && !old_displayed_frame_valid) begin
+                if (displayed_frame_valid) begin
                     state <= WAIT_FOR_CAM;
                     wait_counter <= 0;
                 end
@@ -93,8 +93,18 @@ always_ff @(posedge clk_pixel) begin
 end
 
 
-wire summand_in = detect1;
-wire accum_request_t request_wire = (state == CAPTURE_FRAME) ? WRITE: READ;
+wire summand_in = detect_1;
+accum_request_t request_wire;
+wire request_valid_in = read_request || (good_addrb && active_draw && state == CAPTURE_FRAME);
+wire [ADDRB_DEPTH_WIDTH-1:0] addr_out_wire;
+
+always_comb begin  // https://github.com/steveicarus/iverilog/issues/1015
+    if (state == CAPTURE_FRAME) begin
+        request_wire = WRITE;
+    end else begin
+        request_wire = READ;
+    end
+end
 
 // Instantiat the accum thing
 shift_accum_ram #(
@@ -106,11 +116,11 @@ shift_accum_ram #(
     .addr_in(addrb),
     .summand_in(summand_in),
     .request_type_in(request_wire),
-    .request_valid_in(read_request || (good_addrb && active_draw && state == CAPTURE_FRAME)),
-    .read_out(),
+    .request_valid_in(request_valid_in),
+    .read_out(read_out),
     .summand_out(),
     .sum_out(),
-    .addr_out(),
+    .addr_out(addr_out_wire),
     .request_type_out(),
     .result_valid_out()
 );
