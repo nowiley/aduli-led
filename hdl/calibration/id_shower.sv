e7fc840e7640c0bfff429c2f151210250cb85a1e
Luc Gaitskell <me@lucg.xyz>
2024-12-08 19:14:42 -0500

    Auto-format new modules

--- hdl/calibration/id_shower.sv
+++ hdl/calibration/id_shower.sv
@@ -8,11 +8,10 @@
 // Reset: reset
 // Increment: displays next bit of led address
 // Decrement: displays previous bit of led address
-module id_shower
-# (
+module id_shower #(
     parameter int NUM_LEDS = 50,
     parameter int LED_ADDRESS_WIDTH = 6
-)(
+) (
     input wire clk,
     input wire rst,
     input wire increment_bit,
@@ -34,14 +33,14 @@ module id_shower
     enum logic [1:0] {
         SEEN_ZERO_ZERO_REQUESTS,
         SEEN_ONE_ZERO_REQUESTS,
-        VALID_DISPLAY  
+        VALID_DISPLAY
     } display_state;
 
     assign displayed_frame_valid = (display_state == VALID_DISPLAY);
     logic flag;
 
     always_ff @(posedge clk) begin
-        if (rst) begin // assuming rst is tied to button 0
+        if (rst) begin  // assuming rst is tied to button 0
             green_out <= 0;
             red_out <= 0;
             blue_out <= 0;
@@ -51,40 +50,40 @@ module id_shower
             prev_increment_bit <= increment_bit;
             prev_decrement_bit <= decrement_bit;
             display_state <= SEEN_ZERO_ZERO_REQUESTS;
-        // HANDLE INCREMENT AND DECREMENT
+            // HANDLE INCREMENT AND DECREMENT
         end else if ((increment_bit && !prev_increment_bit) || (decrement_bit && ! prev_decrement_bit)) begin
             prev_increment_bit <= increment_bit;
             prev_decrement_bit <= decrement_bit;
             flag <= 1;
             if (increment_bit && !prev_increment_bit) begin
                 address_bit_num <= address_bit_num + 1;
-                display_state <= SEEN_ZERO_ZERO_REQUESTS;
-            end else if (decrement_bit && ! prev_decrement_bit) begin
+                display_state   <= SEEN_ZERO_ZERO_REQUESTS;
+            end else if (decrement_bit && !prev_decrement_bit) begin
                 address_bit_num <= address_bit_num - 1;
-                display_state <= SEEN_ZERO_ZERO_REQUESTS;
+                display_state   <= SEEN_ZERO_ZERO_REQUESTS;
             end
-        // HANDLE GENERAL CASE
+            // HANDLE GENERAL CASE
         end else begin
             // DISPLAY CURRENT BIT OF REQUEST ADDRESS
-            case (next_led_request[LED_ADDRESS_WIDTH - 1 - address_bit_num])
-            0: begin
-                green_out <= 0;
-            red_out <= 8'hFF;
-                blue_out <= 0;
-                color_valid <= 1;
-            end
-            1: begin
-                green_out <= 0;
-                red_out <= 0;
-                blue_out <= 8'hFF;
-                color_valid <= 1;
-            end
-            default: begin // SHOULD NEVER GO HERE
-                green_out <= 8'hFF;
-                red_out <= 0;
-                blue_out <= 0;
-                color_valid <= 1;
-            end
+            case (next_led_request[LED_ADDRESS_WIDTH-1-address_bit_num])
+                0: begin
+                    green_out <= 0;
+                    red_out <= 8'hFF;
+                    blue_out <= 0;
+                    color_valid <= 1;
+                end
+                1: begin
+                    green_out <= 0;
+                    red_out <= 0;
+                    blue_out <= 8'hFF;
+                    color_valid <= 1;
+                end
+                default: begin  // SHOULD NEVER GO HERE
+                    green_out <= 8'hFF;
+                    red_out <= 0;
+                    blue_out <= 0;
+                    color_valid <= 1;
+                end
             endcase
 
             prev_request <= next_led_request;
@@ -93,12 +92,12 @@ module id_shower
             // accept new requests
             if (next_led_request != prev_request) begin
                 if (prev_request == 0) begin
-                case (display_state)
-                    SEEN_ZERO_ZERO_REQUESTS: display_state <= SEEN_ONE_ZERO_REQUESTS;
-                    SEEN_ONE_ZERO_REQUESTS: display_state <= VALID_DISPLAY;
-                    VALID_DISPLAY: display_state <= VALID_DISPLAY;
-                    default: display_state <= SEEN_ZERO_ZERO_REQUESTS;
-                endcase
+                    case (display_state)
+                        SEEN_ZERO_ZERO_REQUESTS: display_state <= SEEN_ONE_ZERO_REQUESTS;
+                        SEEN_ONE_ZERO_REQUESTS: display_state <= VALID_DISPLAY;
+                        VALID_DISPLAY: display_state <= VALID_DISPLAY;
+                        default: display_state <= SEEN_ZERO_ZERO_REQUESTS;
+                    endcase
                 end
             end
         end
